230bd4dfc4b2b2e9aac673eff196ea6dd4cf6f09
Radhin Al haady <78122715+radhin123@users.noreply.github.com>
2022-10-31 07:12:18 +0000

    update dl-ytv.js

--- plugins/dl-ytv.js
+++ plugins/dl-ytv.js
@@ -1,7 +1,6 @@
 let limit = 50
 let fetch = require('node-fetch')
-const { servers, ytv } = require('../lib/y2mate')
-let handler = async(m, { conn, args, isPrems, isOwner }) => {
+/*let handler = async(m, { conn, args, isPrems, isOwner }) => {
     if (!args || !args[0]) return conn.reply(m.chat, 'Uhm... urlnya mana?', m)
     let chat = db.data.chats[m.chat]
     let server = (args[1] || servers[0]).toLowerCase()
@@ -25,7 +24,19 @@ let handler = async(m, { conn, args, isPrems, isOwner }) => {
      }}
   })
 
+}*/
+/*[download via module di atas]*/
+let { youtubeSearch } = require ('@bochilteam/scraper')
+let handler = async (m, { conn, text }) => {
+  if (!text) throw 'Uhm... urlnya mana?'
+  m.reply('_Proses..._')
+  let vid = (await youtubeSearch(text)).video[0]
+  let { title, description, thumbnail, videoId, durationH, durationS, viewH, publishedTime } = vid
+  let url = 'https://www.youtube.com/watch?v=' + videoId
+let ytLink = `https://botcahx2.ddns.net/?url=${url}&filter=audioandvideo&quality=highestvideo&contenttype=video/mp4`
+  conn.sendMessage(m.chat, { video: { url: ytLink }, mimetype: 'video/mp4' }, { quoted: m })
 }
+/*[supaya suport Railway jadi make module lain]*/
 handler.help = ['ytmp4 <query>']
 handler.tags = ['downloader']
 handler.command = /^yt(v(idi?e?o)?|mp4)?$/i
